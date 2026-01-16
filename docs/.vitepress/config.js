81f3f94db6c191ff55394dd8b4b12c626aef5349
Mauricio Poppe <mauricio.poppe@gmail.com>
2026-01-15 23:36:24 -0500

    docs: Migrate documentation to VitePress structure

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,12 +4,39 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
+      { text: 'GitHub', link: 'https://github.com/mauriciopoppe/kubernetes-playground' }
     ],
     sidebar: [
       {
-        text: 'Guide',
+        text: 'Experiments',
         items: [
-          { text: 'Introduction', link: '/' },
+          { text: 'Sandboxes with Debugger', link: '/sandbox-with-debugger' },
+          { text: 'Controller Runtime', link: '/controller-runtime' },
+        ]
+      },
+      {
+        text: 'Kubernetes Controllers',
+        items: [
+          { text: 'Kube Controller Manager', link: '/kube-controller-manager' },
+          { text: 'Attach/Detach Controller', link: '/attach-detach-controller' },
+          { text: 'PV Controller', link: '/pv-controller' },
+          { text: 'Admission Controller', link: '/admission-controller-default-storage-class' },
+        ]
+      },
+      {
+        text: 'Kubernetes Development',
+        items: [
+          { text: 'General Development', link: '/kubernetes-development' },
+          { text: 'Kubelet', link: '/kubelet' },
+          { text: 'Containerd', link: '/containerd' },
+          { text: 'E2E Tests', link: '/kubernetes-e2e-tests' },
+          { text: 'Unit Tests', link: '/kubernetes-unit-tests' },
+        ]
+      },
+      {
+        text: 'Utils',
+        items: [
+          { text: 'Kubernetes Utils', link: '/kubernetes-utils' },
         ]
       }
     ]
---
+++ docs/index.md
@@ -0,0 +1,23 @@
+---
+layout: home
+
+hero:
+  name: Kubernetes Playground
+  text: Notes & Experiments
+  tagline: Personal knowledge base for Kubernetes internals and development.
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /kubernetes-development
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/mauriciopoppe/kubernetes-playground
+
+features:
+  - title: Core Concepts
+    details: Deep dives into controllers, kubelet, and runtime.
+  - title: Experiments
+    details: Hands-on sandboxes for debugging and testing.
+  - title: Development
+    details: Guides on contributing and developing for Kubernetes.
+---
